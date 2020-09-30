479ed850ef6832b49a265913466262d81adbef47
Nate <37554478+servusDei2018@users.noreply.github.com>
2020-09-30 19:07:30 -0400

    Update main.go
    Edited file and reorganized imports

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/ash9991win/Interpreter/repl"
 	"os"
 	"os/user"
+
+	"github.com/ash9991win/Interpreter/repl"
 )
 
 func main() {
 	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
+	if err != nil { panic(err) }
+
 	fmt.Printf("Hello %s! This is the REPL \n", user.Username)
-	fmt.Printf("Start typing commands! \n")
+	fmt.Println("Start typing commands!")
 	repl.Start(os.Stdin, os.Stdout)
 }
